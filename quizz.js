ec140a1ecb87e3538ec3962dd5e304b618ee0191
JuvitaSaini09 <juvitasaini@gmail.com>
2021-09-01 12:57:35 +0530

    docs:document-quiz.html updated

--- quizz.js
+++ quizz.js
@@ -8,9 +8,9 @@ const rightAnswers = ["90°", "right angled", "60°", "Hypotenuse", "right angle
 function OnClickHandler() {
     let index = 0;
     let scores = 0;
-    const formValues = new FormData(answers)
+    const formValues = new FormData(answers);
     for (let input of formValues.values()) {
-        if ((rightAnswers[index]) == input); {
+        if((rightAnswers[index])=== input) {
             scores = scores + 1;
         }
         index++;
